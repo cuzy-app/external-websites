af0d03d4002a71b1e87fba288e95e2cf3ffb7278
funkycram <webmaster@marc.fun>
2020-12-17 10:58:24 +0100

    update

--- resources/js/humhub.externalWebsites.SpaceSettings.js
+++ resources/js/humhub.externalWebsites.SpaceSettings.js
@@ -12,7 +12,7 @@ humhub.module('externalWebsites.SpaceSettings', function (module, require, $) {
     // If Humhub is embedded in an iframe
     else {
         if (module.config.preventLeavingSpace) {
-            $('html').on('click', 'a[href^="/u/"]', function(e){
+            $('html').on('click', 'a[href*="/u/"], #activityContents a', function(e){
                 e.preventDefault();
             });
         }
